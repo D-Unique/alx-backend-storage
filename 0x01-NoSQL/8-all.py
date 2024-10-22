ad3acbc76455e699bd56f06e7b9cbbd7f6b5016f
Unigwe <emmunigwe@gmail.com>
2024-10-22 23:09:02 +0100

    feat(8-all.py)

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This module contains a Python function
+that lists all documents in a collection
+"""
+
+
+def list_all(mongo_collection):
+    """a Python function that lists all documents in a collection"""
+    result = mongo_collection.find()
+    return result
